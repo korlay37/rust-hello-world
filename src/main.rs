71c8fd93e29bf4bc641920d8ab9faf32a46942bf
korlay37 <ercorlay7_@msn.com>
2024-12-10 20:52:04 -0500

    Implement BufReader for same example

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
 use clap::Parser;
+
 #[derive(Parser)]
 struct Cli {
     pattern: String,
     path: std::path::PathBuf,
-    #[arg(short = 'o', long = "output")]
-    output: std::path::PathBuf
+    // #[arg(short = 'o', long = "output")]
+    // output: std::path::PathBuf
 }
 
 fn main() {
     let args = Cli::parse();
-    println!("pattern: {:?}, path: {:?}, {:?}", args.pattern, args.path, args.output);
+    // let content =  std::fs::read_to_string(&args.path).expect("could not read file");
+    let file = File::open(&args.path).expect("could not open file");
+    let reader = BufReader::new(file);
+
+    
+
+    for line in reader.lines() {
+        let line = line.expect("could not read line");
+        if line.contains(&args.pattern){
+            println!("{}", line);
+        }
+    }
 }
